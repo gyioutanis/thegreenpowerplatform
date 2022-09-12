c5dfc1dd4e276135d47d7fb478ad4ac5c2b340cb
Grigoris Yioutanis <gyioutanis@gmail.com>
2022-09-12 18:34:57 +0300

    ch

--- index.php
+++ index.php
@@ -159,24 +159,19 @@
                                     class="gen-font-gradient">Capabilities</span></h1>
                     </div>
 
-                    <div class="mt-3 text-gray-500 dark:text-gray-300">theGreenpower Platform is now making these
-                        integrated Web3 services available via an API as well as building out further dapps/apps to be
-                        launched into our fast growing userbase around the world and specifically in China where we have
-                        secured a partnership with the first government approved NFT exchange. China is the number 1
-                        market in the world for football with over 400 million avid fans. Below are areas where we are
-                        expanding:
+                    <div class="mt-3 text-gray-500 dark:text-gray-300">Green Power Platform is now making these integrated Web3 services available via an API as well as building out further dapps/apps to be launched into our our base of people around the world who wish empower themselves and their community with cheaper green energy, locally moderated communication and support for small local businesses.
                     </div>
 
                     <div class="de_tab tab_simple tabs" style="background-size: cover;">
 
                         <ul class="col-md-12 de_nav">
 
-                            <li class="active tab-gra ">TFA Wallet and Pay System</li>
-                            <li class="tab-gra">theGreenpower Platform Trivia and Games
+                            <li class="active tab-gra ">NFT based Green Mining</li>
+                            <li class="tab-gra">GPA wallet and pay system
                             </li>
-                            <li class="tab-gra">theGreenpower Platform blockchain ticketing
+                            <li class="tab-gra">GPP pay to earn games
                             </li>
-                            <li class="tab-gra">theGreenpower Platform fan DAO
+                            <li class="tab-gra">GPP DAO capabilities
                             </li>
                         </ul>
                         <div class="de_tab_content" style="background-size: cover;">
@@ -184,25 +179,20 @@
                             <div class="tab-1" style="background-size: cover;">
                                 <div class="md:flex md:items-start md:-mx-4">
                                 <span class="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
-                                         viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/>
-                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                                         stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"/>
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"/>
+                                </svg>
                                 </span>
 
                                     <div class="mt-4 md:mx-4 md:mt-0">
-                                        <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">TFA
-                                            Wallet and Pay System</h1>
+                                        <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">NFT based Green Mining</h1>
 
                                         <p class="mt-3 text-gray-500 dark:text-gray-300">
-                                            theGreenpower Platform makes it easy for non-crypto people to hold and use
-                                            tokens. theGreenpower Platform enables businesses to accept payment with
-                                            tokens while avoiding volatility associated with crypto through its unique
-                                            virtual stable coin credit system. Merchants get FIAT credits in the system
-                                            for sales, then convert those credits to GPC at the moment they cash out -
-                                            so price stays stable. Close to one hundred businesses in Cyprus accept GPC
-                                            in exchange for goods and services.
+                                            Green Power Platform makes it easy for non-crypto people to hold and use digital assets. The platform enables businesses to accept payment with tokens while avoiding volatility associated with crypto through its unique virtual stable coin credit system. Merchants get FIAT credits (Impact credits) in the system for sales, then convert those credits to Green Power Coins (GPC) at the moment they convert to fiat - so price stays stable. Over 100 businesses will accept GPC at launch in exchange for goods and services.
                                         </p>
                                     </div>
                                 </div>
@@ -211,16 +201,18 @@
                             <div class="tab-2" style="background-size: cover;">
                                 <div class="md:flex md:items-start md:-mx-4">
                                 <span class="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
-                                         viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"/>
-                                    </svg>
+                                   <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"/>
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                          d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"/>
+                                </svg>
                                 </span>
 
                                     <div class="mt-4 md:mx-4 md:mt-0">
                                         <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
-                                            theGreenpower Platform Trivia and Games</h1>
+                                            GPA wallet and pay system</h1>
 
                                         <p class="mt-3 text-gray-500 dark:text-gray-300">
                                             TFA has developed some popular games that give staking rewards including:
@@ -264,8 +256,7 @@
                             </span>
 
                                     <div class="mt-4 md:mx-4 md:mt-0">
-                                        <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">TFA
-                                            blockchain ticketing</h1>
+                                        <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">GPP pay to earn games</h1>
 
                                         <p class="mt-3 text-gray-500 dark:text-gray-300">
                                             NFT based ticketing is under development which cannot be counterfeited yet
@@ -292,8 +283,7 @@
                             </span>
 
                                     <div class="mt-4 md:mx-4 md:mt-0">
-                                        <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">TFA
-                                            fan DAO</h1>
+                                        <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">GPP DAO capabilities</h1>
 
                                         <p class="mt-3 text-gray-500 dark:text-gray-300">
                                             A custom set of DAO smart contracts and voting and management interfaces for
